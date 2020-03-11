81149a81adc53789fa6ac1fe2c83964a9eef3f83
Aniruddha Adhikary <aniruddha@saleswhale.com>
2020-03-12 00:10:05 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sobdohash',
+    version='1.0.0',
+    packages=['shobdohash'],
+    url='https://github.com/banglakit/shobdohash',
+    license='MIT',
+    author='The BanglaKit Project and Contributors',
+    author_email='contact@banglakit.org',
+    description=''
+)
